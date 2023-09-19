81aa0f0a991a842b0d7368d9f96e459ef89f1da6
dzdoatclass <doatthanh@gmail.com>
2023-09-19 21:41:45 +0700

    init project

---
+++ footer.php
@@ -0,0 +1,42 @@
+<section class="footer">
+    <div class="box-container">
+
+        <div class="box">
+            <h3>quick links</h3>
+            <a href="home.php">home</a>
+            <a href="about.php">about</a>
+            <a href="shop.php">shop</a>
+            <a href="contact.php">contact</a>
+        </div>
+
+        <div class="box">
+            <h3>extra links</h3>
+            <a href="login.php">login</a>
+            <a href="register.php">register</a>
+            <a href="cart.php">cart</a>
+            <a href="orders.php">orders</a>
+        </div>
+
+
+        <div class="box">
+            <h3>contact</h3>
+            <p><i class=" fas fa-phone" ></i>+8423-456-789</p>
+            <p><i class=" fas fa-phone" ></i>+8423-456-789</p>
+            <p><i class=" fas fa-envelope" ></i>doatthanh@gmail.com</p>
+            <p><i class=" fas fa-map-marker-alt" ></i>520/1 quốc lộ 13, Hiệp Bình Phước, Thủ Đức</p>
+        </div>
+
+        <div class="box">
+            <h3>follow us</h3>
+            
+            <a href="#"><i class="fab fa-facebook-f"></i>facebook</a>
+            <a href="#"><i class="fab fa-twitter"></i>twitter</a>
+            <a href="#"><i class="fab fa-instagram"></i>instagram</a>
+            <a href="#"><i class="fab fa-linkedin"></i>linkedin</a>
+        
+        </div>
+
+    </div>
+    <p class="credit">&copy; careted by <span>Thanh doat </span>@ <?php echo date('Y'); ?> | 
+    all rights reserved</p>
+</section>
